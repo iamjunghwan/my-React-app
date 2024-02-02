2a25c44a27e55e5e6d7145446428b23146920717
dks3344 <dks3344@naver.com>
2024-02-02 19:30:03 +0900

    import path change

--- setup/index.ts
+++ setup/index.ts
@@ -2,8 +2,8 @@ import react from '@vitejs/plugin-react';
 import { PluginOption } from 'vite';
 import { qrcode } from 'vite-plugin-qrcode';
 import Inspect from 'vite-plugin-inspect';
-import setupAutoImport from '~/plugins/setupAutoImport.ts';
-import setupIcon from '~/plugins/setupIcon.ts';
+import setupAutoImport from './plugins/setupAutoImport.ts';
+import setupIcon from './plugins/setupIcon.ts';
 
 export default function setupVitePlugins(
   viteEnv: ViteEnv,
